2229ebc740929886a306d700b36f6513301c17c6
Gonzalo C <gonzalo.cuadros@mango.com>
2025-12-16 23:47:09 +0100

    add remote host

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Habilita PPR + Cache Components
   cacheComponents: true,
+  images: {
+    remotePatterns: [new URL('www.thecocktaildb.com/**')],
+  },
 };
 
 export default nextConfig;
